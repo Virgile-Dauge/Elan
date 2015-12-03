6450e55626a6ce7fa0d26a1288ad753684f3b984
virgileTn <virgile.dauge@telecomnancy.net>
2015-12-03 23:59:55 +0100

    ajout ngMap

--- front/app/js/controler.js
+++ front/app/js/controler.js
@@ -1,14 +1,12 @@
-console.log("poutoux 2 !!! ");
-
-
 app.controller('mainCtrl', ['$scope',function($scope) {
-
-
-    $scope.test = "HELLO WORLD";
-
-    $scope.map = { center: { latitude: 48.85661400000001, longitude: 2.3522219000000177 }, zoom: 8 };
-
-
-    console.log("poutoux 2 !!! ");
-
+  var vm = this;
+  vm.cities = {
+    chicago: {population:2714856, position: [41.878113, -87.629798]},
+    newyork: {population:8405837, position: [40.714352, -74.005973]},
+    losangeles: {population:3857799, position: [34.052234, -118.243684]},
+    vancouver: {population:603502, position: [49.25, -123.1]},
+  }
+  vm.getRadius = function(num) {
+    return Math.sqrt(num) * 100;
+  }
 }]);
