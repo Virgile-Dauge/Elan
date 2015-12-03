85de31da887adc8881a7a7736e934edcfcf32f98
Pierre-Olivier Brissaud <pierre-olivier.brissaud@telecomnancy.fr>
2015-12-03 22:19:21 +0100

    initial commit for front

---
+++ front/app/js/controler.js
@@ -0,0 +1,14 @@
+console.log("poutoux 2 !!! ");
+
+
+app.controller('mainCtrl', ['$scope',function($scope) {
+
+
+    $scope.test = "HELLO WORLD";
+
+    $scope.map = { center: { latitude: 48.85661400000001, longitude: 2.3522219000000177 }, zoom: 8 };
+
+
+    console.log("poutoux 2 !!! ");
+
+}]);
