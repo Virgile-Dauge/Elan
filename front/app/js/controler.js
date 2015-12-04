5b306b466c1c5c0b65b9d02148b558fb7d269bd6
virgileTn <virgile.dauge@telecomnancy.net>
2015-12-04 01:12:04 +0100

    ajout colors et events

--- front/app/js/controler.js
+++ front/app/js/controler.js
@@ -1,11 +1,11 @@
 app.controller('mainCtrl', ['$scope',function($scope) {
   var vm = this;
-  vm.cities = {
-    chicago: {population:2714856, position: [41.878113, -87.629798]},
-    newyork: {population:8405837, position: [40.714352, -74.005973]},
-    losangeles: {population:3857799, position: [34.052234, -118.243684]},
-    vancouver: {population:603502, position: [49.25, -123.1]},
-  }
+  vm.events = [
+    {name:'incendie', color : '#db712b', population:2714856, position: [41.878113, -87.629798]},
+    {name:'tsunami', color : '#2a8bb3', population:8405837, position: [40.714352, -74.005973]},
+    {name:'inondation', color : '#875e1d', population:3857799, position: [34.052234, -118.243684]},
+    {name:'zombie', color : '#1cce20', population:603502, position: [49.25, -123.1]}
+  ];
   vm.getRadius = function(num) {
     return Math.sqrt(num) * 100;
   }
