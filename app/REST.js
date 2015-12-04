0c68562bc103f734f3f9fa7b7c606ff91fb10136
Qu1lby <kilian.cuny@hotmail.fr>
2015-12-04 04:41:22 +0100

    Petit commit

--- app/REST.js
+++ app/REST.js
@@ -37,20 +37,18 @@ module.exports = function (app, client, database, io, router, twitter) {
     });
   })
 
+  /** Get **/
   router.get('/rest/event/tweet/:id', function (req, result) {
     var id_event = req.params.id;
 
-    var myQuery = "SELECT * FROM event WHERE Ev_id = " + id_event + " LIMIT 1";
+    var myQuery = "SELECT Ev_descr FROM event WHERE Ev_id = " + id_event + " LIMIT 1";
     database.executeQuery(myQuery, function (res) {
-      var json = [];
-      json.push(JSON.stringify({
-        date: res[0].Ev_date,
-        lattitude: res[0].Ev_lat,
-        longitude: res[0].Ev_lg,
-        description: res[0].Ev_descr,
-        nb_tweet: res[0].Ev_nb_tweet
-      }));
-      result.send(json);
+      client.get('search/tweets', {
+        q: res[0].Ev_descr,
+        count: 20
+      }, function (error, tweet, response) {
+        result.send(tweet);
+      })
     });
-  })
+  });
 }
