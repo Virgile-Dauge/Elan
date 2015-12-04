c483568e6eca56215c1ed60024f40249e3c86cae
Pierre-Olivier Brissaud <pierre-olivier.brissaud@telecomnancy.fr>
2015-12-04 02:36:09 +0100

    recupération depuis le back

--- front/app/js/controler.js
+++ front/app/js/controler.js
@@ -1,12 +1,15 @@
-app.controller('mainCtrl', ['$scope',function($scope) {
+app.controller('mainCtrl', ['$scope','myService',function($scope,myService) {
   var vm = this;
-  vm.events = [
-    {name:'incendie', color : '#db712b', population:2714856, position: [41.878113, -87.629798]},
-    {name:'tsunami', color : '#2a8bb3', population:8405837, position: [40.714352, -74.005973]},
-    {name:'inondation', color : '#875e1d', population:3857799, position: [34.052234, -118.243684]},
-    {name:'zombie', color : '#1cce20', population:603502, position: [49.25, -123.1]}
-  ];
+
+  myService.callAPI().then(function(response) {
+    console.log(response.data);
+    vm.events = response.data;
+  }, function(error) {
+    console.log(error);
+  });
+
   vm.getRadius = function(num) {
     return Math.sqrt(num) * 100;
   }
+
 }]);
